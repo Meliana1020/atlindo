4088639404b8b276ca28393f7d2caeea90772774
sulthanrizal <surie0312@gmail.com>
2025-01-20 09:33:06 +0700

    add section product

--- src/@components/body/index.jsx
+++ src/@components/body/index.jsx
@@ -1,14 +1,18 @@
 import AboutUsSection from "./section/about-us";
 import HeroSection from "./section/hero"
+import ProductSection from "./section/product";
 
 const Body = () => {
     return (
         <div className="container-body">
-            <section>
+            <section className="section-hero">
                 <HeroSection />
             </section>
-            <section>
+            <section className="section-about">
                 <AboutUsSection />
+            </section>
+            <section className="section-product">
+                <ProductSection />
             </section>
         </div>
     )
---
+++ src/@components/body/section/product/index.jsx
@@ -0,0 +1,9 @@
+import './index.scss'
+const ProductSection = () => {
+    return (
+        <section className="container-product">
+            <div>Product</div>
+        </section>
+    )
+}
+export default ProductSection;
