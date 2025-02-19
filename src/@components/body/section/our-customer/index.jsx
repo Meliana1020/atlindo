88e3c0c26475496353d202e96ad7568c9225a8d2
sulthanrizal <surie0312@gmail.com>
2025-02-19 17:40:02 +0700

    revisi in mobile

--- src/@components/body/section/our-customer/index.jsx
+++ src/@components/body/section/our-customer/index.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
 import { customer } from "../../../data/dami-data";
 import './index.scss'
 
 const OurCustomerSection = () => {
+    const [getWitdh, setGetWitdh] = useState({
+        width: undefined
+    })
+
+    useEffect(() => {
+
+        function handleResize() {
+            const witdh = document.documentElement.clientWidth
+            setGetWitdh(witdh)
+        }
+        window.addEventListener('resize', handleResize)
+        handleResize()
+        return () => window.addEventListener('resize', handleResize)
+    }, [])
+
     return (
         <section className="container-our-customer marg-all" >
             <div className="header-our-customer" >
@@ -15,7 +31,11 @@ const OurCustomerSection = () => {
                         return (
                             <div className="list-customer" key={index} >
                                 <div className="col-customer">
-                                    <img src={img} style={id !== 'ssk' ? { height: '30px' } : { height: '50px' }} />
+                                    <img src={img} style={{
+                                        height: getWitdh < 700
+                                            ? (item?.id !== 'ssk' ? '30px' : '50px')
+                                            : '60px'
+                                    }} />
                                 </div>
                             </div>
                         )
